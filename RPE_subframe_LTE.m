c58c48ad7015899bad1fe8bbf3315d3a94413603
konkapo <kostaskapogl@gmail.com>
2022-01-26 05:47:24 +0200

    Fixed RPE_subframe_LTE.m

--- RPE_subframe_LTE.m
+++ RPE_subframe_LTE.m
@@ -10,34 +10,27 @@ function [N,b] = RPE_subframe_LTE(d,Prevd)
 
 %% Init Variables
 lamda_max = length(Prevd);
-subframe_size = len(d);
+subframe_size = length(d);
 R = zeros(1,lamda_max-subframe_size);
-d_doubleStress = zeros(1,subframe_size);
-d_Stress = zeros(1,subframe_size);
+d_doubleStress = zeros(subframe_size, 1);
+d_Stress = zeros(subframe_size, 1);
 
 %% 3.1.13 page 25 Estimation
 % 1)
-for lamda = subframe_size:lamda_max
-    R(lamda) = xcorr(d,Prevd(lamda-subframe_size+1:lamda));    
-end
-
+% for lamda = subframe_size:lamda_max
+%     R(lamda-subframe_size+1) = sum(d(:).*Prevd(lamda-subframe_size+1:lamda));    
+% end
+R = xcorr(Prevd,d, 80);
+R = R(81:end);
 % 2)
-[~,I] = max(R);
-N = I+subframe_size-1;
+[~,N] = max(R);
 
 % 3)
 % b = R(N)/(sum(Prevd((end-N):(end-N+39))^2))
-b = R(N)/xcorr(Prevd(N-subframe_size+1:N));
+b = R(N)/sum(Prevd(N:N+subframe_size).*Prevd(N:N+subframe_size));
 
 %% 3.1.16 page 27 Prediction
-for k=1:subframe_size
-    if ( k < N )
-        d_doubleStress(k) = b.*Prevd(subframe_size -N+k);
-    else
-        d_doubleStress(k) = d(k-N); %when Prevd ends and we take values from the current frame
-        %nomizo thne xreiazetai
-    end
-end
+d_doubleStress(:) = b*Prevd(N+1:N+subframe_size);
 e = d-d_doubleStress;
 
 %% 3.1.17 Synthesis
--- demo.m
+++ demo.m
@@ -1,16 +1,19 @@
-[frame, Fs] = audioread('car.wav');
-MinLARc = [-32 -32 -16 -16 -8 -8 -4 -4];
-MaxLARc = [31 31 15 15 7 7 3 3];
-s0 = zeros(128000,1);
-for i = 1:1
+ [frame, Fs] = audioread('car.wav');
+% MinLARc = [-32 -32 -16 -16 -8 -8 -4 -4];
+% MaxLARc = [31 31 15 15 7 7 3 3];
+% s0 = zeros(128000,1);
+% for i = 1:1
+% [LARc,  CurrFrmSTResd]= findLARc(frame((i-1)*160+1:(i*160)));
+% s0((i-1)*160+1:(i*160)) = RPE_frame_ST_decoder(LARc, CurrFrmSTResd);
+% end
+i = 1;
 [LARc,  CurrFrmSTResd]= findLARc(frame((i-1)*160+1:(i*160)));
-s0((i-1)*160+1:(i*160)) = RPE_frame_ST_decoder(LARc, CurrFrmSTResd);
-end
-
-audiowrite('car2.wav',s0,Fs)
-figure(3)
-plot(s0)
-hold on
-plot(frame)
-legend('s0','frame')
-title('Decoded signal and frame comparison')
+[N,b] = RPE_subframe_LTE(CurrFrmSTResd(121:160),CurrFrmSTResd(1:120))
+% 
+% audiowrite('car2.wav',s0,Fs)
+% figure(3)
+% plot(s0)
+% hold on
+% plot(frame)
+% legend('s0','frame')
+% title('Decoded signal and frame comparison')
