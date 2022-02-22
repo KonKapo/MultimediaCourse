07d83f5f1d8b08f4a6103c9b3a78d4843091dfd7
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-02-22 21:14:27 +0200

    Complteted demo for SLT.
    Come back here if you need to demonstrate SLT coding-decoding

--- demo.m
+++ demo.m
@@ -3,24 +3,27 @@
 % MaxLARc = [31 31 15 15 7 7 3 3];
 % s0 = zeros(128000,1);
 for i = 1:800
+    [LARc,  CurrFrmSTResd]= RPE_frame_ST_coder(frame((i-1)*160+1:(i*160)));
     if i == 1
-        CurrFrmSTResd=frame((i-1)*160+1:(i*160));
-        PrevFrmSTResd = zeros(1,160);
-        [LARc, Nc,bc,CurrFrmExFull,CurrFrmSTResd]= RPE_frame_SLT_coder(CurrFrmSTResd,PrevFrmSTResd);
+        [Nc,bc,CurrFrmExFull,CurrFrmSTResd]= RPE_frame_SLT_coder(CurrFrmSTResd,CurrFrmSTResd);
     else
-        [LARc, Nc,bc,CurrFrmExFull,CurrFrmSTResd]= RPE_frame_SLT_coder(CurrFrmSTResd,PrevFrmSTResd');
+        [Nc,bc,CurrFrmExFull,CurrFrmSTResd]= RPE_frame_SLT_coder(CurrFrmSTResd,PrevFrmSTResd');
     end
-    s0((i-1)*160+1:(i*160)) = RPE_frame_ST_decoder(LARc, CurrFrmSTResd);
+    if i == 1
+        s0((i-1)*160+1:(i*160)) = RPE_frame_SLT_decoder(LARc,Nc,bc, CurrFrmSTResd,CurrFrmSTResd);
+    else
+        s0((i-1)*160+1:(i*160)) = RPE_frame_SLT_decoder(LARc,Nc,bc, CurrFrmSTResd,PrevFrmSTResd');
+    end
     PrevFrmSTResd = CurrFrmSTResd;
 end
 % i = 1;
-%[LARc,  CurrFrmSTResd]= findLARc(frame((i-1)*160+1:(i*160)));
-%[N,b] = RPE_subframe_LTE(CurrFrmSTResd(121:160),CurrFrmSTResd(1:120));
+% [LARc,  CurrFrmSTResd]= findLARc(frame((i-1)*160+1:(i*160)));
+% [N,b] = RPE_subframe_LTE(CurrFrmSTResd(121:160),CurrFrmSTResd(1:120));
 % 
-% audiowrite('car2.wav',s0,Fs)
-% figure(3)
-% plot(s0)
-% hold on
-% plot(frame)
-% legend('s0','frame')
-% title('Decoded signal and frame comparison')
+audiowrite('car2.wav',s0,Fs)
+figure(3)
+plot(s0)
+hold on
+plot(frame)
+legend('s0','frame')
+title('Decoded signal and frame comparison')
