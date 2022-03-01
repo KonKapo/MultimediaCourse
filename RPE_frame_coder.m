b5bcd1b48e413243998d346c2c946097ed24962b
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-03-01 15:36:22 +0200

    Minor fixes
    Fixed LAR_Stress(i,4)
    rc2poly cut first element
    FrmBitStrm build

--- RPE_frame_coder.m
+++ RPE_frame_coder.m
@@ -1,5 +1,5 @@
 function [FrmBitStrm, CurrFrmResd] = RPE_frame_coder(s0,PrevFrmResd)
-[LARc,Nc,bc,CurrFrmExFull,CurrFrmSTResd]  = RPE_frame_SLT_coder(s0, PrevFrmResd);
+% [LARc,Nc,bc,CurrFrmExFull,CurrFrmSTResd]  = RPE_frame_SLT_coder(s0, PrevFrmResd);
 
 %[LARc, CurrFrmSTResd] = RPE_frame_SLT_coder(s0, PrevFrmSTResd)to add
 % Additions 3.1.9 implementation
@@ -10,9 +10,6 @@ function [FrmBitStrm, CurrFrmResd] = RPE_frame_coder(s0,PrevFrmResd)
 frame = s0;
 l = length(frame);
 s_pred = zeros(1,l);
-d_1D = zeros(1,l);
-d = zeros(9,l);
-u = zeros(9,l);
 ACF = zeros(1,9);
 LAR = zeros(8,1);
 LARc = zeros(8,1);
@@ -30,7 +27,6 @@ R = ACF(1:end-1);
 R = toeplitz(R);
 r = ACF(2:end)';
 w = mldivide(R,r);
-r2=w;
 r = poly2rc([1 -w(1) -w(2) -w(3) -w(4) -w(5) -w(6) -w(7) -w(8)]);
 for i=1:8
     if(abs(r(i))<0.675)
@@ -64,45 +60,127 @@ for i=1:8
     LAR_Stress(i,1) = 0.75*PrevLAR_doubleStress(i) + 0.25*LAR_doubleStress(i);
     LAR_Stress(i,2) = 0.5*PrevLAR_doubleStress(i) + 0.5*LAR_doubleStress(i);
     LAR_Stress(i,3) = 0.25*PrevLAR_doubleStress(i) + 0.75*LAR_doubleStress(i);
-    LAR_Stress(i,4) = 0*PrevLAR_doubleStress(i) + 0.25*LAR_doubleStress(i);
+    LAR_Stress(i,4) = 0*PrevLAR_doubleStress(i) + 1*LAR_doubleStress(i);
     %% 3.1.10
     % equation (3.5)
     for k = 1:l
-        if(abs(LAR_Stress(i,l))<0.675)
-            r(i,l) = LAR_Stress(i,l);
-        elseif(abs(LAR_Stress(i))>=0.675 && abs(LAR_Stress(i,l))<1.225)
-            r(i,l)=sign(LAR_Stress(i,l))*(0.5*abs(LAR_Stress(i,l)) + 0.3375);
-        else
-            r(i,l)=sign(LAR_Stress(i,l))*(0.125*abs(LAR_Stress(i,l)) + 0.796875);
+        for j = 1:4
+            if(abs(LAR_Stress(i,j))<0.675)
+                r(i,j) = LAR_Stress(i,j);
+            elseif(abs(LAR_Stress(i,j))>=0.675 && abs(LAR_Stress(i,j))<1.225)
+                r(i,j)=sign(LAR_Stress(i,j))*(0.5*abs(LAR_Stress(i,j)) + 0.3375);
+            else
+                r(i,j)=sign(LAR_Stress(i,j))*(0.125*abs(LAR_Stress(i,j)) + 0.796875);
+            end
         end
     end
     
 end
-r(:,1) = -rc2poly(r(:,1));
-r(:,2) = -rc2poly(r(:,2));
-r(:,3) = -rc2poly(r(:,3));
-r(:,4) = -rc2poly(r(:,4));
+
+a = -rc2poly(r(:,1));
+r(:,1) = a(2:end);
+a = -rc2poly(r(:,2));
+r(:,2) = a(2:end);
+a = -rc2poly(r(:,3));
+r(:,3) = a(2:end);
+a = -rc2poly(r(:,4));
+r(:,4) = a(2:end);
 
 for k = 1:l
     %% 3.1.11
-    d(1,k) = frame(k);
-    u(1,k) = frame(k);
-    for i=2:9
-        if (k-i-1>0)
-            if (k<=12)
-                s_pred(k) = s_pred(k) + r(k)*frame(k-i-1);
-            elseif (k>12 && k<=27)
-                s_pred(k) = s_pred(k) + r(k)*frame(k-i-1);
-            elseif (k>27 && k<=40)
-                s_pred(k) = s_pred(k) + r(k)*frame(k-i-1);
-            else
-                s_pred(k) = s_pred(k) + r(k)*frame(k-i-1);
-            end
-        end
+    if (k<=13)
+        s_pred(1:13) = filter(r(i,1), 1, frame(1:13)');
+    elseif (k>13 && k<=27)
+        s_pred(14:27) = filter(r(i,2), 1, frame(14:27)');
+    elseif (k>27 && k<=40)
+        s_pred(28:40) = filter(r(i,3), 1, frame(28:40)');
+    else
+        s_pred(41:end) = filter(r(i,4), 1, frame(41:end)');
     end
+        
+    
 end
 CurrFrmSTResd = frame-s_pred';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if(sum(PrevFrmResd==0)~=160)
+    s0 = CurrFrmSTResd;
+    %% init vars
+    Nc = zeros(1,4);
+    DLB = [0.2 0.5 0.8];
+    bc = zeros(1,4);
+    
+    %% Prediction
+    prevd = PrevFrmResd(40:160);
+    [N,b] = RPE_subframe_LTE(s0(1:40),prevd);
+    Nc(1)=N;
+    
+    %% 3.1.15 sub-segment number 1
+    if (b<=DLB(1))
+        bc(1)=0;
+    elseif (DLB(1)<b && b<=DLB(2))
+        bc(1)=1;
+    elseif(DLB(2)<b && b<=DLB(3))
+        bc(1)=2;
+    elseif(DLB(3)<b)
+        bc(1)=3;
+    end
+    %% 3.1.16 sub-segment number 1
+    CurrFrmSTResd(1:40)= bc(1)*prevd(Nc(1):Nc(1)+39)';
+    prevd = [PrevFrmResd(80:160)' CurrFrmSTResd(1:40)'];
+    
+    %% 3.1.15 sub-segment number 2
+    [N,b] = RPE_subframe_LTE(s0(41:80),prevd');
+    Nc(2)=N;
+    if (b<=DLB(1))
+        bc(2)=0;
+    elseif (DLB(1)<b && b<=DLB(2))
+        bc(2)=1;
+    elseif(DLB(2)<b && b<=DLB(3))
+        bc(2)=2;
+    elseif(DLB(3)<b)
+        bc(2)=3;
+    end
+    %% 3.1.16 sub-segment number 2
+    CurrFrmSTResd(41:80)= bc(2)*prevd(Nc(2):Nc(2)+39);
+    prevd = [PrevFrmResd(120:160)' CurrFrmSTResd(1:80)'];
+    
+    %% 3.1.15 sub-segment number 3
+    [N,b] = RPE_subframe_LTE(s0(81:120),prevd');
+    Nc(3)=N;
+    
+    if (b<=DLB(1))
+        bc(3)=0;
+    elseif (DLB(1)<b && b<=DLB(2))
+        bc(3)=1;
+    elseif(DLB(2)<b && b<=DLB(3))
+        bc(3)=2;
+    elseif(DLB(3)<b)
+        bc(3)=3;
+    end
+    %% 3.1.16 sub-segment number 3
+    CurrFrmSTResd(81:120)= bc(3)*prevd(Nc(3):Nc(3)+39);
+    
+    %% 3.1.15 sub-segment number 4
+    [N,b] = RPE_subframe_LTE(s0(121:160), CurrFrmSTResd(1:120));
+    Nc(4)=N;
+    if (b<=DLB(1))
+        bc(4)=0;
+    elseif (DLB(1)<b && b<=DLB(2))
+        bc(4)=1;
+    elseif(DLB(2)<b && b<=DLB(3))
+        bc(4)=2;
+    elseif(DLB(3)<b)
+        bc(4)=3;
+    end
+    %% 3.1.16 sub-segment number 4
+    CurrFrmSTResd(121:160)= bc(4)*prevd(Nc(4):Nc(4)+39);
+else
+    Nc = [1 1 1 1];
+    bc = [1 1 1 1];
+end
+CurrFrmExFull(:) = s0 - CurrFrmSTResd;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 CurrFrmResd = CurrFrmSTResd;
 H = [-134 -374 0 2054 5471 8192 5471 2054 0 -374 -134];
 H = H / 2^13;
@@ -148,5 +226,5 @@ for j = 0:3
         x_max = 16384+x_maxc*2048-1;
     end
     x_tone=xmax/x_max;
-    FrmBitStrm=0;
+    FrmBitStrm=[Nc bc LARc'];
 end
