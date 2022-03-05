8b512697d09bb971ff8781da3cc9257fe5c5284a
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-03-05 06:01:21 +0200

    LAR correction

--- RPE_frame_decoder.m
+++ RPE_frame_decoder.m
@@ -14,14 +14,79 @@ xMax = zeros(4,1);
 x_tone = zeros(4,13);
 xd = zeros(160,1);
 x = zeros(160,1);
-LARc(1) = bin2dec(num2str(FrmBitStrm(1:6)));
-LARc(2) = bin2dec(num2str((FrmBitStrm(7:12))));
-LARc(3) = bin2dec(num2str((FrmBitStrm(13:17))));
-LARc(4) = bin2dec(num2str((FrmBitStrm(18:22))));
-LARc(5) = bin2dec(num2str((FrmBitStrm(23:26))));
-LARc(6) = bin2dec(num2str((FrmBitStrm(27:30))));
-LARc(7) = bin2dec(num2str((FrmBitStrm(31:33))));
-LARc(8) = bin2dec(num2str((FrmBitStrm(34:36))));
+
+LARb = FrmBitStrm(1:36);
+if(LARb(1)==0)
+    LARc(1) = bin2dec(num2str(FrmBitStrm(2:6)));
+elseif(FrmBitStrm(1)==1 && FrmBitStrm(2)==0 && FrmBitStrm(3)==0 && FrmBitStrm(4)==0 && FrmBitStrm(5)==0 && FrmBitStrm(6)==0)
+    LARc(1) = -32;
+elseif(LARb(1)==1)
+    LARc(1) = -bin2dec(num2str(FrmBitStrm(2:6)));
+end
+if(LARb(7)==0)
+    LARc(2) = bin2dec(num2str(FrmBitStrm(8:12)));
+elseif(FrmBitStrm(7)==1 && FrmBitStrm(8)==0 && FrmBitStrm(9)==0 && FrmBitStrm(10)==0 && FrmBitStrm(11)==0 && FrmBitStrm(12)==0)
+    LARc(2) = -32;
+elseif(LARb(7)==1)
+    LARc(2) = -bin2dec(num2str(FrmBitStrm(8:12)));
+end
+
+if(LARb(13)==0)
+    LARc(3) = bin2dec(num2str(FrmBitStrm(14:17)));
+elseif(FrmBitStrm(13)==1 && FrmBitStrm(14)==0 &&FrmBitStrm(15)==0 && FrmBitStrm(16)==0 && FrmBitStrm(17)==0 )
+    LARc(3) = -16;
+elseif(LARb(13)==1)
+    LARc(3) = -bin2dec(num2str(FrmBitStrm(14:17)));
+end
+
+if(LARb(18)==0)
+    LARc(4) = bin2dec(num2str(FrmBitStrm(19:22)));
+elseif(FrmBitStrm(18)==1 && FrmBitStrm(19)==0 &&FrmBitStrm(20)==0 && FrmBitStrm(21)==0 && FrmBitStrm(22)==0 )
+    LARc(4) = -16;
+elseif(LARb(18)==1)
+    LARc(4) = -bin2dec(num2str(FrmBitStrm(19:22)));
+end
+
+if(LARb(23)==0)
+    LARc(5) = bin2dec(num2str(FrmBitStrm(24:26)));
+elseif(FrmBitStrm(23)==1 && FrmBitStrm(24)==0 &&FrmBitStrm(25)==0 && FrmBitStrm(26)==0)
+    LARc(5) = -8;
+elseif(LARb(23)==1)
+    LARc(5) = -bin2dec(num2str(FrmBitStrm(24:26)));
+end
+
+if(LARb(27)==0)
+    LARc(6) = bin2dec(num2str(FrmBitStrm(28:30)));
+elseif(FrmBitStrm(27)==1 && FrmBitStrm(28)==0 &&FrmBitStrm(29)==0 && FrmBitStrm(30)==0)
+    LARc(6) = -8;
+elseif(LARb(27)==1)
+    LARc(6) = -bin2dec(num2str(FrmBitStrm(28:30)));
+end
+
+if(LARb(31)==0)
+    LARc(7) = bin2dec(num2str(FrmBitStrm(32:33)));
+elseif(FrmBitStrm(31)==1 && FrmBitStrm(32)==0 &&FrmBitStrm(33)==0)
+    LARc(7) = -4;
+elseif(LARb(31)==1)
+    LARc(7) = -bin2dec(num2str(FrmBitStrm(32:33)));
+end
+
+if(LARb(34)==0)
+    LARc(8) = bin2dec(num2str(FrmBitStrm(35:36)));
+elseif(FrmBitStrm(34)==1 && FrmBitStrm(35)==0 &&FrmBitStrm(36)==0)
+    LARc(8) = -4;
+elseif(LARb(34)==1)
+    LARc(8) = -bin2dec(num2str(FrmBitStrm(35:36)));
+end
+
+% LARc(1) = bin2dec(num2str(FrmBitStrm(1:6)));
+% LARc(2) = bin2dec(num2str((FrmBitStrm(7:12))));
+% LARc(3) = bin2dec(num2str((FrmBitStrm(13:17))));
+% LARc(4) = bin2dec(num2str((FrmBitStrm(18:22))));
+% LARc(5) = bin2dec(num2str((FrmBitStrm(23:26))));
+% LARc(6) = bin2dec(num2str((FrmBitStrm(27:30))));
+% LARc(7) = bin2dec(num2str((FrmBitStrm(31:33))));
+% LARc(8) = bin2dec(num2str((FrmBitStrm(34:36))));
 
 Nc(1) = bin2dec(num2str((FrmBitStrm(37:43))));
 Nc(2) = bin2dec(num2str((FrmBitStrm(93:99))));
@@ -88,7 +153,7 @@ for l = 1:4
 end
 
 x_tone2 = x_toned./2^15;
-Mc
+Mc;
 for j = 1:4
     x_new(j,:) = x_tone2(j,:)* x_maxd(j);
     xd(Mc(j)+40*(j-1):3:40*(j-1)+40-4+Mc(j)) = x_new(j,:);
@@ -106,4 +171,5 @@ end
 % end
 x = xd./2^13;
 [s0, CurrFrmResd] = RPE_frame_SLT_decoder(LARc,Nc,bc,x', PrevFrmResd);
+
 end
