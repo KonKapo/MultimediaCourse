3c5fb3c4a30a820ae79d487fdd5f581805bedd11
Christos Skapetis <45330616+Skapis9999@users.noreply.github.com>
2022-03-05 03:41:38 +0200

    graphs

--- demo1.m
+++ demo1.m
@@ -1,21 +1,64 @@
 [frame, Fs] = audioread('car.wav');
 s0 = zeros(128000,1);
-e=zeros(800,1);
-PrevLARc=zeros(8,1);
 for i = 1:800
     [LARc, CurrFrmSTResd] = RPE_frame_ST_coder(frame((i-1)*160+1:(i*160)));
     s0((i-1)*160+1:(i*160)) = RPE_frame_ST_decoder(LARc, CurrFrmSTResd);
-    PrevLARc=LARc;
-    e(i) = std(s0((i-1)*160+1:(i*160)) - frame((i-1)*160+1:(i*160)));
 end
-audiowrite('car2.wav',s0,Fs)
+audiowrite('car2.wav',s0/max(abs(s0)),Fs)
 
 figure(1)
-plot(s0,'y')
+clf
+a = s0/max(abs(s0));
+plot(a,'r')
 hold on
-plot(frame,'c')
-legend('s0','frame')
+b = frame/max(abs(frame));
+plot(b,'c')
+legend('Reconstructed Signal s0','Initial Signal')
 title('Decoded signal and frame comparison')
+
+e = frame/(max(abs((frame)))) - s0/(max(abs(s0)));
 figure(2)
-stem(e)
-std(e)
+clf
+plot(e)
+legend('Reconstructed Signal - Initial Signal')
+title('Normalised Reconstruction Error')
+
+e2 = frame - s0;
+figure(3)
+clf
+plot(e2)
+legend('Reconstructed Signal - Initial Signal')
+title('Reconstruction Error')
+
+se = e/sqrt(length(s0));
+figure(4)
+clf
+plot(se)
+legend('Reconstructed Signal - Initial Signal')
+title('Reconstruction Standard Error')
+
+pRMS = rms(se).^2
+% Power of the Reconstruction Standard Error
+% 3.2468e-08
+energy = sum(se.^2)
+% Energy of the Reconstruction Standard Error
+% 0.0042
+
+figure(5)
+clf
+obw(frame,1000e7)
+figure(6)
+clf
+obw(frame)
+% figure(5)
+% clf
+% plot(obw(frame))
+% title('99% Occupied Bandwidth')
+
+
+
+
+
+
+
+
